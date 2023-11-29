b2236b24b71a7e7ffaf2b4e54ef6d565451cfe2e
demondev0666 <demondev0666@gmail.com>
2023-11-28 22:25:37 -0500

    Episode 1 Complete!

---
+++ index.js
@@ -0,0 +1,16 @@
+//console.log(`Hello`);
+//console.log(`World!`);
+
+//window.alert(`this is an alert`);
+
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = `I Like Pizza`;
+// This is a comment
+
+/* 
+   This 
+   Is
+   A
+   Comment
+   */
